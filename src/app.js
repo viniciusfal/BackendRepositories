41bff24a77df3a41765ca3f9c56ef2338a6560df
Vinicius Silva <vinicius_prsilva@hotmail.com>
2021-01-09 00:25:34 -0300

    Delete-added

--- src/app.js
+++ src/app.js
@@ -44,6 +44,10 @@ app.put("/repositories/:id", (request, response) => {
 
   const repositoryIndex = repositories.findIndex(repository => repository.id == id);
 
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: 'Project not a found'});
+  }
+
   const repository = {
     id,
     title,
@@ -57,7 +61,17 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({error: 'Project not a found'})
+  };
+
+  repositories.splice(repositoryIndex, 1);
+  
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
